5348363b1499fc70756cff78b58821d01348f544
Leon <bdleon71@gmail.com>
2024-05-27 01:33:50 +0600

    Object & Event

--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ const leon ={
     age:27,
     color : "Dark",
     fullName : function (){
-        console.log(this.fastName + " " + this.lastName);
+        return this.fastName + " " + this.lastName;
     },
     job: function(){
         console.log("CableONE");
@@ -15,5 +15,22 @@ const leon ={
 };
 
 
-leon.fullName();
+leon.job();
 console.log(leon.age);
+
+
+
+const phone ={
+    brandName : "Realme",
+    modelName : "Note 50",
+    phoneType : "Android",
+    vertionOs : 13,
+    ram : "4gb",
+    rom : "128gb",
+    display : "LED HD"
+
+};
+document.getElementById('PhoneModel').innerHTML = leon.fullName();
+function timeShow(){
+    document.getElementById('btnd').innerHTML = Date();
+};
